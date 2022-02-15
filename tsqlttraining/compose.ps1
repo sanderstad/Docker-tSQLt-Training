8a3815a9b10c85aba2726856ecbdd8606ef21282
Sander Stad <sanderstad@gmail.com>
2022-02-15 10:19:56 +0100

    Removed images

--- tsqlttraining/compose.ps1
+++ tsqlttraining/compose.ps1
@@ -13,53 +13,19 @@ docker builder prune -a -f
 docker compose up --force-recreate --build --remove-orphans -d
 
 # push out to docker hub
-docker push sanderstad/tsqlttraining:latest-amd64
+docker push sanderstad/tsqlttraining:latest
 
 # Remove manifest cache
 Remove-Item $HOME/.docker/manifests -Recurse -Force
 
 # Create manifests that support  multiple architectures
-docker manifest create sanderstad/tsqlttraining:latest --amend sanderstad/tsqlttraining:latest-amd64 --amend sanderstad/tsqlttraining:latest-arm64
+docker manifest create sanderstad/tsqlttraining:latest
 
 # view it if you want
 docker manifest inspect docker.io/sanderstad/tsqlttraining:latest
 
 # push out to docker
 docker manifest push docker.io/sanderstad/tsqlttraining:latest --purge
-
-<#
-
-
-        FOR ARM DEPLOYMENTS ON M1 or Raspberry Pi
-
-
-#>
-# rebuild the whole thing with no caches
-docker compose down --volumes
-docker volume prune -f
-docker image prune -f
-docker builder prune -a -f
-docker compose up --force-recreate --build --remove-orphans -d
-
-# retag image
-docker tag sanderstad/tsqlttraining sanderstad/tsqlttraining:latest-arm64
-
-# push out to docker hub
-docker push sanderstad/tsqlttraining:latest-arm64
-
-# Remove manifest cache
-Remove-Item $HOME/.docker/manifests -Recurse -Force
-
-# Create manifests that support  multiple architectures
-docker manifest create sanderstad/tsqlttraining:latest --amend sanderstad/tsqlttraining:latest-amd64 --amend sanderstad/tsqlttraining:latest-arm64
-
-# view it if you want
-docker manifest inspect docker.io/sanderstad/tsqlttraining:latest
-
-# push out to docker
-docker manifest push docker.io/sanderstad/tsqlttraining:latest --purge
-
-
 
 <#
 # Clean up!
