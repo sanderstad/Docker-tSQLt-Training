df465d8b42908218afe2581b8998ac2428f6de70
Sander Stad <sanderstad@gmail.com>
2022-02-15 10:11:56 +0100

    Removed unused image

--- tsqlttraining/compose.ps1
+++ tsqlttraining/compose.ps1
@@ -14,28 +14,18 @@ docker compose up --force-recreate --build --remove-orphans -d
 
 # push out to docker hub
 docker push sanderstad/tsqlttraining:latest-amd64
-docker push sanderstad/tsqlttraining2:latest-amd64
 
 # Remove manifest cache
 Remove-Item $HOME/.docker/manifests -Recurse -Force
 
 # Create manifests that support  multiple architectures
 docker manifest create sanderstad/tsqlttraining:latest --amend sanderstad/tsqlttraining:latest-amd64 --amend sanderstad/tsqlttraining:latest-arm64
-docker manifest create sanderstad/tsqlttraining2:latest --amend sanderstad/tsqlttraining2:latest-amd64 --amend sanderstad/tsqlttraining2:latest-arm64
 
 # view it if you want
 docker manifest inspect docker.io/sanderstad/tsqlttraining:latest
-docker manifest inspect docker.io/sanderstad/tsqlttraining2:latest
 
 # push out to docker
 docker manifest push docker.io/sanderstad/tsqlttraining:latest --purge
-docker manifest push docker.io/sanderstad/tsqlttraining2:latest --purge
-
-
-
-
-
-
 
 <#
 
@@ -53,26 +43,21 @@ docker compose up --force-recreate --build --remove-orphans -d
 
 # retag image
 docker tag sanderstad/tsqlttraining sanderstad/tsqlttraining:latest-arm64
-docker tag sanderstad/tsqlttraining2 sanderstad/tsqlttraining2:latest-arm64
 
 # push out to docker hub
 docker push sanderstad/tsqlttraining:latest-arm64
-docker push sanderstad/tsqlttraining2:latest-arm64
 
 # Remove manifest cache
 Remove-Item $HOME/.docker/manifests -Recurse -Force
 
 # Create manifests that support  multiple architectures
 docker manifest create sanderstad/tsqlttraining:latest --amend sanderstad/tsqlttraining:latest-amd64 --amend sanderstad/tsqlttraining:latest-arm64
-docker manifest create sanderstad/tsqlttraining2:latest --amend sanderstad/tsqlttraining2:latest-amd64 --amend sanderstad/tsqlttraining2:latest-arm64
 
 # view it if you want
 docker manifest inspect docker.io/sanderstad/tsqlttraining:latest
-docker manifest inspect docker.io/sanderstad/tsqlttraining2:latest
 
 # push out to docker
 docker manifest push docker.io/sanderstad/tsqlttraining:latest --purge
-docker manifest push docker.io/sanderstad/tsqlttraining2:latest --purge
 
 
 
