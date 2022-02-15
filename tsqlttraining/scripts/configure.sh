a3c46274c36cb66abc30c7f8d561a290dd8de968
Sander Stad <sanderstad@gmail.com>
2022-02-15 14:32:02 +0100

    Fixed database references Added more restores Added tsqlt install

--- tsqlttraining/scripts/configure.sh
+++ tsqlttraining/scripts/configure.sh
@@ -8,12 +8,25 @@ do
 done
 
 # create sqladmin with dbatools.IO password and disable sa
-sqlcmd -S localhost -d master -i /tmp/create-admin.sql
+/opt/mssql-tools/bin/sqlcmdsqlcmd -S localhost -d master -i /tmp/create-admin.sql
 
 # change the default login to sqladmin instead of sa
 export SQLCMDUSER=sqladmin
 
 # download the example databases
-wget https://github.com/sanderstad/Training-Day-SQL-Bits-2022/raw/main/tsqlttraining/resources/northwind.bak
-wget https://github.com/sanderstad/Training-Day-SQL-Bits-2022/raw/main/tsqlttraining/resources/pubs.bak
-sqlcmd -S localhost -d master -i /tmp/restore-db.sql -U sqladmin -P MyStr0ngP@ssword
+wget https://github.com/sanderstad/Docker-tSQLt-Training/raw/main/tsqlttraining/resources/northwind.bak
+wget https://github.com/sanderstad/Docker-tSQLt-Training/raw/main/tsqlttraining/resources/pubs.bak
+
+# restore the databases
+/opt/mssql-tools/bin/sqlcmd -S localhost -d master -i /tmp/restore-db.sql -U sa -P MyStr0ngP@ssword
+
+# prepare instance for tSQLt
+/opt/mssql-tools/bin/sqlcmd -S localhost -d master -i /tmp/tsqlt/PrepareServer.sql -U sa -P MyStr0ngP@ssword
+
+# install tsqlt into example databases
+/opt/mssql-tools/bin/sqlcmd -S localhost -d Northwind-tSQLt -i /tmp/tsqlt/tSQLt.class.sql -U sa -P MyStr0ngP@ssword
+/opt/mssql-tools/bin/sqlcmd -S localhost -d pubs-tSQLt -i /tmp/tsqlt/tSQLt.class.sql -U sa -P MyStr0ngP@ssword
+
+#install tSQLt extra example database
+/opt/mssql-tools/bin/sqlcmd -S localhost -d master -i /tmp/tsqlt/Example.sql -U sa -P MyStr0ngP@ssword
+
